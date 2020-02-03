508e80d1e80e49dbb5632196e03c0f80a733b1da
Roman Wilhelm <roman.wilhelm@ledger.fr>
2020-02-03 16:48:59 +0100

    BACK-417 : Pass pwd to pg url when is provided

--- src/main/scala/co/ledger/wallet/daemon/models/Pool.scala
+++ src/main/scala/co/ledger/wallet/daemon/models/Pool.scala
@@ -267,8 +267,32 @@ object Pool {
 
   def newCoreInstance(poolDto: PoolDto): Future[core.WalletPool] = {
     val poolConfig = core.DynamicObject.newInstance()
-    //    poolConfig.putString("BLOCKCHAIN_OBSERVER_WS_ENDPOINT", "ws://notification.explorers.dev.aws.ledger.fr:9000/ws/{}")
-    //    poolConfig.putString("BLOCKCHAIN_OBSERVER_ENGINE", "LEDGER_API")
+    val dbBackend = Try(config.getString("core_database_engine")).toOption.getOrElse("sqlite3") match {
+      case "postgres" =>
+        val dbName = for {
+          dbPort <- Try(config.getString("postgres.port"))
+          dbHost <- Try(config.getString("postgres.host"))
+          dbUserName <- Try(config.getString("postgres.username"))
+          dbPwd <- Try(config.getString("postgres.password"))
+          dbPrefix <- Try(config.getString("postgres.db_name_prefix"))
+        } yield {
+          // Ref: postgres://USERNAME:PASSWORD@HOST:PORT/DBNAME
+          if (dbPwd.isEmpty) {
+            s"postgres://${dbUserName}@${dbHost}:${dbPort}/${dbPrefix}${poolDto.name}"
+          } else {
+            s"postgres://${dbUserName}:${dbPwd}@${dbHost}:${dbPort}/${dbPrefix}${poolDto.name}"
+          }
+        }
+        dbName match {
+          case Success(value) =>
+            poolConfig.putString("DATABASE_NAME", value)
+            core.DatabaseBackend.getPostgreSQLBackend(config.getInt("postgres.pool_size"))
+          case Failure(exception) =>
+            throw CoreDatabaseException("Failed to configure wallet daemon's core database", exception)
+        }
+      case _ => core.DatabaseBackend.getSqlite3Backend
+    }
+
     core.WalletPoolBuilder.createInstance()
       .setHttpClient(ClientFactory.httpClient)
       .setWebsocketClient(ClientFactory.webSocketClient)
